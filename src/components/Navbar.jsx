dd7efe9a8e64cfd99c0e47f394bb65f2d95e35e7
adiekisma <adiek.isma@gmail.com>
2023-01-25 16:14:31 +0700

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,20 +7,24 @@ const Navbar = () => {
 
     const links = [
         {
-        id: 1,
-        link: 'home'
+            id: 1,
+            link: 'home'
         },
         {
-        id: 2,
-        link: 'about'
+            id: 2,
+            link: 'about'
         },
         {
-        id: 3,
-        link: 'portofolio'
+            id: 3,
+            link: 'portofolio'
         },
         {
-        id: 4,
-        link: 'contact'
+            id: 4,
+            link: 'experience'
+        },
+        {
+            id: 5,
+            link: 'contact'
         },
     ]
 
