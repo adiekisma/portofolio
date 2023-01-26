087be184b6b47f2e791a3e6862bf2fd54fe4470e
adiekisma <adiek.isma@gmail.com>
2023-01-26 13:50:55 +0700

    update exp

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -11,11 +11,29 @@ const Contact = () => {
 
                 <div className='flex justify-center items-center'>
                     <form action="" className='flex flex-col w-full md:w-1/2'>
-                        <input type="text" name='name' placeholder='Enter your name' className='p-2 bg-transparent border-2 rounded-md text-white focus:outline-none' />
-                        <input type="text" name='email' placeholder='Enter your e-mail' className='my-4 p-2 bg-transparent border-2 rounded-md text-white focus:outline-none' />
-                        <textarea name="message" rows="10" className='p-2 bg-transparent border-2 rounded-md text-white focus:outline-none'></textarea>
+                        <input 
+                            type="text" 
+                            name='name' 
+                            placeholder='Enter your name' 
+                            className='p-2 bg-transparent border-2 rounded-md text-white focus:outline-none' 
+                        />
+                        <input 
+                            type="text" 
+                            name='email' 
+                            placeholder='Enter your e-mail' 
+                            className='my-4 p-2 bg-transparent border-2 rounded-md text-white focus:outline-none' 
+                        />
+                        <textarea 
+                            name="message" 
+                            rows="10" 
+                            className='p-2 bg-transparent border-2 rounded-md text-white focus:outline-none'>
+                        </textarea>
 
-                        <button className='text-white bg-gradient-to-b from-cyan-400 to-blue-400 px-6 py-3 my-8 mx-auto flex items-center rounded-md hover:scale-150 duration-500'>Let's Talk</button>
+                        <button 
+                        className='text-white bg-gradient-to-b from-cyan-400 to-blue-400 px-6 py-3 my-8 mx-auto flex items-center rounded-md hover:scale-150 duration-500'
+                        >
+                            Let's Talk
+                        </button>
                     </form>
                 </div>
             </div>
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -8,8 +8,11 @@ const Home = () => {
         <div name="home" className='h-screen w-full bg-gradient-to-b from-black via-black to-gray-800 text-white'>
             <div className='max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row'>
                 <div className='flex flex-col justify-center h-full'>
-                    <h2 className='text-4xl sm:text-7xl font-bold'>Hello I'm FullStack Developer</h2>
-                    <p className='text-gray-300 py-4 max-w-md'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae optio veritatis atque molestias quas accusantium. Assumenda temporibus illo molestias nostrum ipsam quod architecto unde ab, cum ratione similique eius adipisci!
+                    <h2 className='text-4xl sm:text-7xl font-bold'>
+                        Hello I'm FullStack Developer
+                    </h2>
+                    <p className='text-gray-300 py-4 max-w-md'>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae optio veritatis atque molestias quas accusantium. Assumenda temporibus illo molestias nostrum ipsam quod architecto unde ab, cum ratione similique eius adipisci!
                     </p>
 
                     <div>
